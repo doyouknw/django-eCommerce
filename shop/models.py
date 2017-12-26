cb1cd4ba0e08fafd032f69e3e7b349b93225d1fc
Anupam Kumar <poison_k9@live.com>
2017-12-26 15:15:43 +0530

    UI modification

--- shop/models.py
+++ shop/models.py
@@ -15,17 +15,3 @@ class Product(models.Model):
     picture = models.CharField(max_length=30)
     color = models.CharField(max_length=10)
 
-class UserAccount(models.Model):
-    username = models.CharField(max_length=30)
-    password = models.CharField(max_length=100)
-    reg_date = models.DateField(default=datetime.date.today)
-    last_login = models.DateField(default=datetime.date.today)
-    block = models.BooleanField(default=True)
-    
-class User(models.Model):
-    user = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
-    fullname = models.CharField(max_length=30)
-    email = models.CharField(max_length=50)
-    dob = models.DateField(blank=True, null=True)
-    phone = models.CharField(max_length=15, blank=True, null=True)
-    address = models.TextField(blank=True, null=True)
--- shop/views.py
+++ shop/views.py
@@ -3,6 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     if request.user.is_authenticated():
-        username = request.user.username
-        return render(request, "index.html", {'username' : username})
+        return render(request, "index.html", {})
     return render(request, "index.html", {})
